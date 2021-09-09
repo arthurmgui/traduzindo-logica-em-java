1d62d6df0d2574b538bfc127ea33bc3be7f016e8
Arthur <arthurmottaguimaraes@outlook.com>
2021-09-09 16:47:43 -0300

    declaração simples de números inteiros

--- src/traduzindo/logica/em/java/TraduzindoLogicaEmJava.java
+++ src/traduzindo/logica/em/java/TraduzindoLogicaEmJava.java
@@ -6,7 +6,12 @@ public class TraduzindoLogicaEmJava {
 
 //        # Tipos Primitivos e Manipulação de Dados
 //        ....
-//
+//        -soma de números inteiros simples
+        int v1 = 5;
+        int v2 = 5;
+        int soma = v1 + v2;
+        System.out.println("A soma de "+ v1 +" com "+ v2 +" é: "+ soma +"." );
+        
 //        # Operadores Aritméticos e Classe Math
 //        ....
 //
@@ -21,7 +26,6 @@ public class TraduzindoLogicaEmJava {
 //
 //        # Estruturas de Repetição
 //        ....
-
     }
     
 }
