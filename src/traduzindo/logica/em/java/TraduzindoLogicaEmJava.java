f0c26038cf90cf69a88137dd5d9c71e6235182c9
Arthur <arthurmottaguimaraes@outlook.com>
2021-09-09 17:39:33 -0300

    acrescentando interação com o usuáro

--- src/traduzindo/logica/em/java/TraduzindoLogicaEmJava.java
+++ src/traduzindo/logica/em/java/TraduzindoLogicaEmJava.java
@@ -1,5 +1,7 @@
 package traduzindo.logica.em.java;
 
+import java.util.Scanner;
+
 public class TraduzindoLogicaEmJava {
 
     public static void main(String[] args) {
@@ -7,10 +9,14 @@ public class TraduzindoLogicaEmJava {
 //        # Tipos Primitivos e Manipulação de Dados
 //        ....
 //        -soma de números inteiros simples
-        int v1 = 5;
-        int v2 = 5;
+        Scanner scan = new Scanner (System.in);
+        System.out.println("Qual o seu nome? ");
+        String nome = scan.nextLine();
+        System.out.println("Agora me diga dois números pressionando a tecla enter.");
+        int v1 = scan.nextInt();
+        int v2 = scan.nextInt();
         int soma = v1 + v2;
-        System.out.println("A soma de "+ v1 +" mais "+ v2 +" é "+ soma +"." );
+        System.out.println("A soma de "+ v1 +" mais "+ v2 +" é "+ soma +", "+ nome +".");
         
 //        # Operadores Aritméticos e Classe Math
 //        ....
