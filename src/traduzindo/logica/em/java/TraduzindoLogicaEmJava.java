4addf4132b8f16f85adf9c1041a1d1a66de9898f
Arthur <arthurmottaguimaraes@outlook.com>
2021-09-10 16:14:22 -0300

    exemplificando tipos de variáveis.

--- src/traduzindo/logica/em/java/TraduzindoLogicaEmJava.java
+++ src/traduzindo/logica/em/java/TraduzindoLogicaEmJava.java
@@ -9,14 +9,29 @@ public class TraduzindoLogicaEmJava {
 //        # Tipos Primitivos e Manipulação de Dados
 //        ....
 //        -soma de números inteiros simples
-        Scanner scan = new Scanner (System.in);
-        System.out.println("Qual o seu nome? ");
-        String nome = scan.nextLine();
-        System.out.println("Agora me diga dois números pressionando a tecla enter.");
-        int v1 = scan.nextInt();
-        int v2 = scan.nextInt();
-        int soma = v1 + v2;
-        System.out.println("A soma de "+ v1 +" mais "+ v2 +" é "+ soma +", "+ nome +".");
+//        Scanner scan = new Scanner (System.in);
+//        System.out.println("Qual o seu nome? ");
+//        String nome = scan.nextLine();
+//        System.out.println("Agora me diga dois números pressionando a tecla enter.");
+//        int v1 = scan.nextInt();
+//        int v2 = scan.nextInt();
+//        int soma = v1 + v2;
+//        System.out.println("A soma de "+ v1 +" mais "+ v2 +" é "+ soma +", "+ nome +".");
+//        
+//        -lista com exemplos:
+        System.out.println("Tipos de dados em Java: \n" +
+	            "\nMenor Byte: " + Byte.MIN_VALUE +
+	            "\nMaior Byte: " + Byte.MAX_VALUE +
+	            "\nMenor Short Int: " + Short.MIN_VALUE +
+	            "\nMaior Short Int: " + Short.MAX_VALUE +
+	            "\nMenor Int: " + Integer.MIN_VALUE +
+	            "\nMaior Int: " + Integer.MAX_VALUE +
+	            "\nMenor Long: " + Long.MIN_VALUE +
+	            "\nMaior Long:" + Long.MAX_VALUE +
+	            "\nMenor Float: " + Float.MIN_VALUE +
+	            "\nMaior Float: " + Float.MAX_VALUE +
+	            "\nMenor Double: " + Double.MIN_VALUE +
+	            "\nMaior Double: " + Double.MAX_VALUE);
         
 //        # Operadores Aritméticos e Classe Math
 //        ....
