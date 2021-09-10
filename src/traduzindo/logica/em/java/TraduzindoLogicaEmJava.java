192024b64675fcfbdd83c62b6fe4cf78877c3eef
Arthur <arthurmottaguimaraes@outlook.com>
2021-09-10 18:14:04 -0300

    corrigindo o código feita

--- src/traduzindo/logica/em/java/TraduzindoLogicaEmJava.java
+++ src/traduzindo/logica/em/java/TraduzindoLogicaEmJava.java
@@ -19,35 +19,37 @@ public class TraduzindoLogicaEmJava {
 //                System.out.println("A soma de "+ v1 +" mais "+ v2 +" é "+ soma +", "+ nome +".");
 //                           
 //            -lista com exemplos:
-//                  System.out.println("Tipos de dados em Java: \n" +
-//                      "\nMenor Byte: " + Byte.MIN_VALUE +
-//        	        "\nMaior Byte: " + Byte.MAX_VALUE +
-//        	        "\nMenor Short Int: " + Short.MIN_VALUE +
-//        	        "\nMaior Short Int: " + Short.MAX_VALUE +
-//        	        "\nMenor Int: " + Integer.MIN_VALUE +
-//        	        "\nMaior Int: " + Integer.MAX_VALUE +
-//        	        "\nMenor Long: " + Long.MIN_VALUE +
-//        	        "\nMaior Long:" + Long.MAX_VALUE +
-//        	        "\nMenor Float: " + Float.MIN_VALUE +
-//        	        "\nMaior Float: " + Float.MAX_VALUE +
-//        	        "\nMenor Double: " + Double.MIN_VALUE +
-//        	        "\nMaior Double: " + Double.MAX_VALUE);
-//                  System.out.println("Tipos de dados em Java: \n" +
-//                        short tipoShort = 32767;
-//                        char tipoChar = 'C';
-//                        float tipoFloat = 2.6f;
-//                        double tipoDouble = 3.59;
-//                        int tipoInt = 2147483647;
-//                        long tipoLong = 9223372036854775807L;
-//                        boolean tipoBooleano = true;
-//                        System.out.println("Valor do tipoByte = " + tipoByte);
-//                        System.out.println("Valor do tipoShort = " + tipoShort);
-//                        System.out.println("Valor do tipoChar = " + tipoChar);
-//                        System.out.println("Valor do tipoFloat = " + tipoFloat);
-//                        System.out.println("Valor do tipoDouble = " + tipoDouble);
-//                        System.out.println("Valor do tipoInt = " + tipoInt);
-//                        System.out.println("Valor do tipoLong = " + tipoLong);
-//                        System.out.println("Valor do tipoBooleano = " + tipoBooleano);
+                  System.out.println("Tipos de dados em Java: \n" +
+                      "\nMenor Byte: " + Byte.MIN_VALUE +
+        	        "\nMaior Byte: " + Byte.MAX_VALUE +
+        	        "\nMenor Short Int: " + Short.MIN_VALUE +
+        	        "\nMaior Short Int: " + Short.MAX_VALUE +
+        	        "\nMenor Int: " + Integer.MIN_VALUE +
+        	        "\nMaior Int: " + Integer.MAX_VALUE +
+        	        "\nMenor Long: " + Long.MIN_VALUE +
+        	        "\nMaior Long:" + Long.MAX_VALUE +
+        	        "\nMenor Float: " + Float.MIN_VALUE +
+        	        "\nMaior Float: " + Float.MAX_VALUE +
+        	        "\nMenor Double: " + Double.MIN_VALUE +
+        	        "\nMaior Double: " + Double.MAX_VALUE);
+
+                        byte tipoByte = 127;
+                        short tipoShort = 32767;
+                        char tipoChar = 'C';
+                        float tipoFloat = 2.6f;
+                        double tipoDouble = 3.59;
+                        int tipoInt = 2147483647;
+                        long tipoLong = 9223372036854775807L;
+                        boolean tipoBooleano = true;
+                        System.out.println("Valor do tipoByte = " + tipoByte);
+                        System.out.println("Valor do tipoShort = " + tipoShort);
+                        System.out.println("Valor do tipoChar = " + tipoChar);
+                        System.out.println("Valor do tipoFloat = " + tipoFloat);
+                        System.out.println("Valor do tipoDouble = " + tipoDouble);
+                        System.out.println("Valor do tipoInt = " + tipoInt);
+                        System.out.println("Valor do tipoLong = " + tipoLong);
+                        System.out.println("Valor do tipoBooleano = " + tipoBooleano);
+                        
 //        # Operadores Aritméticos, Operadores Lógicos, Relacionais, Condicionais
 //        ....
 //
