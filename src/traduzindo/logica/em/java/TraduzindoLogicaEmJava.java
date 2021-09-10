c05b4ef6bb25b096e09009f0e4c5ccc0eb23a95f
Arthur <arthurmottaguimaraes@outlook.com>
2021-09-10 18:01:55 -0300

    exemplos de tipos primitivos

--- src/traduzindo/logica/em/java/TraduzindoLogicaEmJava.java
+++ src/traduzindo/logica/em/java/TraduzindoLogicaEmJava.java
@@ -7,42 +7,51 @@ public class TraduzindoLogicaEmJava {
     public static void main(String[] args) {
 
 //        # Tipos Primitivos e Manipulação de Dados
-//        ....
-//        -soma de números inteiros simples
-//        Scanner scan = new Scanner (System.in);
-//        System.out.println("Qual o seu nome? ");
-//        String nome = scan.nextLine();
-//        System.out.println("Agora me diga dois números pressionando a tecla enter.");
-//        int v1 = scan.nextInt();
-//        int v2 = scan.nextInt();
-//        int soma = v1 + v2;
-//        System.out.println("A soma de "+ v1 +" mais "+ v2 +" é "+ soma +", "+ nome +".");
-//        
-//        -lista com exemplos:
-        System.out.println("Tipos de dados em Java: \n" +
-	            "\nMenor Byte: " + Byte.MIN_VALUE +
-	            "\nMaior Byte: " + Byte.MAX_VALUE +
-	            "\nMenor Short Int: " + Short.MIN_VALUE +
-	            "\nMaior Short Int: " + Short.MAX_VALUE +
-	            "\nMenor Int: " + Integer.MIN_VALUE +
-	            "\nMaior Int: " + Integer.MAX_VALUE +
-	            "\nMenor Long: " + Long.MIN_VALUE +
-	            "\nMaior Long:" + Long.MAX_VALUE +
-	            "\nMenor Float: " + Float.MIN_VALUE +
-	            "\nMaior Float: " + Float.MAX_VALUE +
-	            "\nMenor Double: " + Double.MIN_VALUE +
-	            "\nMaior Double: " + Double.MAX_VALUE);
-        
-//        # Operadores Aritméticos e Classe Math
+
+//            -soma de números inteiros simples
+//                Scanner scan = new Scanner (System.in);
+//                System.out.println("Qual o seu nome? ");
+//                String nome = scan.nextLine();
+//                System.out.println("Agora me diga dois números pressionando a tecla enter.");
+//                    int v1 = scan.nextInt();
+//                    int v2 = scan.nextInt();
+//                    int soma = v1 + v2;
+//                System.out.println("A soma de "+ v1 +" mais "+ v2 +" é "+ soma +", "+ nome +".");
+//                           
+//            -lista com exemplos:
+//                  System.out.println("Tipos de dados em Java: \n" +
+//                      "\nMenor Byte: " + Byte.MIN_VALUE +
+//        	        "\nMaior Byte: " + Byte.MAX_VALUE +
+//        	        "\nMenor Short Int: " + Short.MIN_VALUE +
+//        	        "\nMaior Short Int: " + Short.MAX_VALUE +
+//        	        "\nMenor Int: " + Integer.MIN_VALUE +
+//        	        "\nMaior Int: " + Integer.MAX_VALUE +
+//        	        "\nMenor Long: " + Long.MIN_VALUE +
+//        	        "\nMaior Long:" + Long.MAX_VALUE +
+//        	        "\nMenor Float: " + Float.MIN_VALUE +
+//        	        "\nMaior Float: " + Float.MAX_VALUE +
+//        	        "\nMenor Double: " + Double.MIN_VALUE +
+//        	        "\nMaior Double: " + Double.MAX_VALUE);
+//                  System.out.println("Tipos de dados em Java: \n" +
+//                        short tipoShort = 32767;
+//                        char tipoChar = 'C';
+//                        float tipoFloat = 2.6f;
+//                        double tipoDouble = 3.59;
+//                        int tipoInt = 2147483647;
+//                        long tipoLong = 9223372036854775807L;
+//                        boolean tipoBooleano = true;
+//                        System.out.println("Valor do tipoByte = " + tipoByte);
+//                        System.out.println("Valor do tipoShort = " + tipoShort);
+//                        System.out.println("Valor do tipoChar = " + tipoChar);
+//                        System.out.println("Valor do tipoFloat = " + tipoFloat);
+//                        System.out.println("Valor do tipoDouble = " + tipoDouble);
+//                        System.out.println("Valor do tipoInt = " + tipoInt);
+//                        System.out.println("Valor do tipoLong = " + tipoLong);
+//                        System.out.println("Valor do tipoBooleano = " + tipoBooleano);
+//        # Operadores Aritméticos, Operadores Lógicos, Relacionais, Condicionais
 //        ....
 //
-//        # Operadores Lógicos
-//        ....
-//
-//        # Operadores Relacionais
-//        ....
-//
-//        # Estruturas Condicionais
+//        #Class Math
 //        ....
 //
 //        # Estruturas de Repetição
