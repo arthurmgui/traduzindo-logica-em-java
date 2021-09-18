baf1b1f8794a4f4aa02fd28934926f900c636989
Arthur <arthurmottaguimaraes@outlook.com>
2021-09-18 17:22:41 -0300

    criando função para o array e acrescentando do while no método main

--- src/traduzindo/logica/em/java/TraduzindoLogicaEmJava.java
+++ src/traduzindo/logica/em/java/TraduzindoLogicaEmJava.java
@@ -2,6 +2,7 @@ package traduzindo.logica.em.java;
 
 import java.util.Scanner;
 
+
 public class TraduzindoLogicaEmJava {
 	
 	public static void operadoresAritmeticos() {
@@ -24,6 +25,7 @@ public class TraduzindoLogicaEmJava {
         int valorA = scan.nextInt();
         System.out.print("Segundo valor: ");
         int valorB = scan.nextInt();
+        
         if(valorA == valorB){
             System.out.println("Os valores são iguais.");
         }else if(valorA > valorB){
@@ -38,7 +40,6 @@ public class TraduzindoLogicaEmJava {
     	Scanner scan = new Scanner (System.in);
 		System.out.println("Quer namorar comigo? [Sim/Não/Talvez]");
 		String querNamorarComigo = scan.nextLine();
-		
 		
 		if(
 			( querNamorarComigo.equals("Sim") ) ||
@@ -65,6 +66,7 @@ public class TraduzindoLogicaEmJava {
     	Scanner scan = new Scanner (System.in);
         System.out.println("Quer namorar comigo? [Sim/Não/Talvez]");
         String querNamorarComigo = scan.nextLine();
+        
         switch(querNamorarComigo) {
             case "Sim":
                 System.out.println("Ela me ama! :D");
@@ -82,6 +84,7 @@ public class TraduzindoLogicaEmJava {
 	}
 
     public static void estruturasDeRepeticao() {
+
     	System.out.println("Digite um número para escolher qual estrutura vai usar: \n"+
     	    	"\n[1] while."+
     	    	"\n[2] do while."+
@@ -121,63 +124,71 @@ public class TraduzindoLogicaEmJava {
 		}
     }
     
+    public static void array() {
+		Scanner scan = new Scanner (System.in);
+		System.out.println("Quantas pessoas participam da mentoria?");
+		int quantAluno = scan.nextInt();
+		String listAluno[] = new String[quantAluno];
+		
+		for (int c = 0; c < quantAluno; c++){
+			System.out.println("Qual o nome do aluno?");
+			String nomeAluno = scan.next();
+			listAluno[c] = nomeAluno;
+		}
+		System.out.println("=======LISTA DE ALUNOS=======");
+		
+		for (int c = 0; c < quantAluno; c++){
+			System.out.println( (c + 1) + "-" + listAluno[c] + ".");
+		}
+		
+		//------------------------------------------------------------------------
+		
+		//for (int cont = 0; cont <= vetor.Length-1 ;cont++);
+		//{
+		//System.out.print(vetor[cont]);	
+		//}
+    }
+    
     public static void main(String[] args) {
     	
     	Scanner scan = new Scanner (System.in);
-    	System.out.println("Olá! Digite um número para escolher o contúedo desejado em java: \n"+
-    	"\n[1] Operadores Aritméticos."+
-    	"\n[2] Operadores Relacionais."+
-    	"\n[3] Operadores Lógicos."+
-    	"\n[4] Estruturas Condicionais."+
-    	"\n[5] Estruturas de repetição.");
-    	int desejo = scan.nextInt();
-    	
-    	switch(desejo) {
-	    	case (1):
-	    		operadoresAritmeticos();
-	    	break;
-	    	case (2):
-	    		operadoresRelacionais();
-	    	break;
-	    	case (3):
-	    		operadoresLogicos();
-	    	break;
-	    	case (4):
-	    		estruturasCondicionais();
-	    	break;
-	    	case (5):
-	    		estruturasDeRepeticao();
-	    	break;
+    	String resp;
+    	do {
+	    	System.out.println("Olá! Digite um número para escolher o contúedo desejado em java: \n"+
+	    	"\n[1] Operadores Aritméticos."+
+	    	"\n[2] Operadores Relacionais."+
+	    	"\n[3] Operadores Lógicos."+
+	    	"\n[4] Estruturas Condicionais."+
+	    	"\n[5] Estruturas de repetição."+
+	    	"\n[6] Array.");
+	    	int desejo = scan.nextInt();
+	    	
+	    	switch(desejo) {
+		    	case (1):
+		    		operadoresAritmeticos();
+		    	break;
+		    	case (2):
+		    		operadoresRelacionais();
+		    	break;
+		    	case (3):
+		    		operadoresLogicos();
+		    	break;
+		    	case (4):
+		    		estruturasCondicionais();
+		    	break;
+		    	case (5):
+		    		estruturasDeRepeticao();
+		    	break;
+		    	case (6):
+		    		array();
+		    	break;
+	    	}
+	    	System.out.println("Deseja continuar?");
+	    	System.out.println("[sim/não]");
+	    	resp = scan.next();
     	}
-        
-            
-    		
+    	while (resp.equals("sim"));
 			
-    	//-Array
-			
-    		//Scanner scan = new Scanner (System.in);
-			//System.out.println("Quantas pessoas participam da mentoria?");
-			//int quantAluno = scan.nextInt();
-			//String listAluno[] = new String[quantAluno];
-			
-			//for (int c = 0; c < quantAluno; c++)
-			//{
-				//System.out.println("Qual o nome do aluno?");
-				//String nomeAluno = scan.next();
-				//listAluno[c] = nomeAluno;
-			//}
-			//System.out.println("=======LISTA DE ALUNOS=======");
-			//for (int c = 0; c < quantAluno; c++)
-			//{
-				//System.out.println( (c + 1) + "-" + listAluno[c] + ".");
-			//}
-
-    		//------------------------------------------------------------------------
-    		
-			//for (int cont = 0; cont <= vetor.Length-1 ;cont++);
-			//{
-			//System.out.print(vetor[cont]);	
-			//}
-        }
+   }
     	
 }
